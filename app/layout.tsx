2f0516443007b5bedd697245d7c92028fa26fd3e
ewomatc <iamgreatdiro@gmail.com>
2024-09-20 17:12:59 +0100

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Space_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import ToastProvider from "@/components/providers/toast-provider";
+
+const spacemono = Space_Mono({
+	variable: "--font-geist-sans",
+	weight: ["400", "700"],
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "e71 LMS - Fix that skill issue you autistic monkey 🫵🏻",
+	description:
+		"Course learning platform by e7i, inspiration from codewithantonio(YT).",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${spacemono.className} antialiased`}>
+					<ToastProvider />
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
+	);
+}
